7c868e89265fd45b560abcec53343f1560234f73
thedanielvogelsang <dr.vogelsang26@gmail.com>
2017-06-19 15:20:12 -0500

    changes counter

--- web_guesser.rb
+++ web_guesser.rb
@@ -2,17 +2,32 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 SECRET_NUMBER = rand(101)
+@@counter = 6
+@@game_over = false
 
 get '/' do
-  guess = params[:guess].to_i if params[:guess]
-  message, css_color = check_guess(guess)
-  erb :index, :locals => {:number => SECRET_NUMBER, :message => message, :css_color => css_color}
+  cheat_mode = params[:cheat]
+  message, css_color = check_guess(params[:guess].to_i) if params[:guess]
+  if @@counter == 1 && @@game_over == false
+    SECRET_NUMBER = rand(101)
+    @@counter = 5
+    message = "You lost!"
+    css_color = "red"
+  elsif @@game_over
+    SECRET_NUMBER = rand(101)
+    @@game_over = false
+    @@counter = 6
+  else
+    @@counter -= 1
+  end
+  erb :index, :locals => {:number => SECRET_NUMBER, :message => message, :css_color => css_color, :counter => @@counter, cheat: cheat_mode}
 end
 
 def check_guess(guess)
   case
   when guess == SECRET_NUMBER
-    ["You got it right!\nThe Secret number is #{SECRET_NUMBER}", "green"]
+    @@game_over = true
+    ["You got it right!\nThe Secret number is #{SECRET_NUMBER}. Let's play again!", "green"]
   when guess > SECRET_NUMBER + 5
     ["Way too high!", "#E74C3C"]
   when guess > SECRET_NUMBER
